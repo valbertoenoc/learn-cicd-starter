75e274b082190810c92222b6860f552ae5bcfe2c
Valberto Enoc <valbertoenoc@gmail.com>
2026-09-09 19:04:33 -0300

    my first changes (#1)
    * my first changes
    
    * force failure basic ci
    
    * swapped force failure step with go version
    
    * add tests to auth package; run tests in ci workflow
    
    * add coverage report
    
    * add ci badge to README

---
+++ internal/auth/get_api_key_test.go
@@ -0,0 +1,48 @@
+package auth
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+	"strings"
+)
+
+func TestGetAPI(t *testing.T) {
+	tests := []struct{
+		key string
+		value string
+		expect string
+		expectErr string
+	}{
+		{
+			key: "Authorization",
+			value: "-",
+			expectErr: "malformed authorization header",
+		},
+	}
+
+	for i, tt := range tests {
+		t.Run(fmt.Sprintf("TestGetAPIKey case %d", i), func(t *testing.T) {
+			header := http.Header{}
+			header.Add(tt.key, tt.value)
+
+			output, err := GetAPIKey(header)
+			if err != nil {
+				if strings.Contains(err.Error(), tt.expectErr) {
+					return
+				}
+				t.Errorf("Unexpected: TestGetAPIKey:%v\n", err)
+				return
+			}
+
+			if output != tt.expect {
+				t.Errorf("Unexpected: TestGetAPIKey:%s", output)
+				return
+			}
+			
+		})
+
+	} 
+	
+	fmt.Printf("tests: %v", tests)
+}
